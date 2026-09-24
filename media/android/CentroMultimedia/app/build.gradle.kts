28381dec96e3afcb23ead0c01ace34945e9a8bbc
gpcorex <gpcorex@gmail.com>
2026-09-24 12:46:45 -0300

    Add Centro Multimedia Android GitHub build

---
+++ media/android/CentroMultimedia/app/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "org.central.media"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.central.media"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
+
+kotlinOptions {
+    jvmTarget = "17"
+}
